1d7e28f9cf5d6d1893adc453ba319ebd3a6f5ee2
Otto Kekäläinen <otto@seravo.fi>
2015-05-03 23:09:46 +0300

    Imported Upstream version 5.5.43

--- storage/innobase/dict/dict0dict.c
+++ storage/innobase/dict/dict0dict.c
@@ -2530,13 +2530,16 @@ dict_foreign_remove_from_cache(
 
 		rbt = foreign->referenced_table->referenced_rbt;
 
-		if (rbt != NULL) {
+		if (rbt != NULL && foreign->id != NULL) {
 			const ib_rbt_node_t*	node
 				= rbt_lookup(rbt, foreign->id);
-			dict_foreign_t*	val = *(dict_foreign_t**) node->value;
 
-			if (val == foreign) {
-				rbt_delete(rbt, foreign->id);
+			if (node) {
+				dict_foreign_t*	val = *(dict_foreign_t**) node->value;
+
+				if (val == foreign) {
+					rbt_delete(rbt, foreign->id);
+				}
 			}
 		}
 	}
@@ -2549,13 +2552,16 @@ dict_foreign_remove_from_cache(
 			       foreign);
 		rbt = foreign->foreign_table->foreign_rbt;
 
-		if (rbt != NULL) {
+		if (rbt != NULL && foreign->id != NULL) {
 			const ib_rbt_node_t*	node
 				= rbt_lookup(rbt, foreign->id);
-			dict_foreign_t*	val = *(dict_foreign_t**) node->value;
+			if (node) {
 
-			if (val == foreign) {
-				rbt_delete(rbt, foreign->id);
+				dict_foreign_t*	val = *(dict_foreign_t**) node->value;
+
+				if (val == foreign) {
+					rbt_delete(rbt, foreign->id);
+				}
 			}
 		}
 	}
